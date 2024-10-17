3f98f31d28f0b63d244e561bd491b4b5af8073cc
sugiyama1692 <kyoei.engine.mba2408.starlight@gmail.com>
2024-10-17 20:43:07 +0900

    スライドショーアプリ

---
+++ SlideshowApp/SliderCellCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SliderCellCollectionViewCell.swift
+//  SlideshowApp
+//
+//  Created by mba2408.starlight kyoei.engine on 2024/10/17.
+//
+
+import UIKit
+
+class SliderCellCollectionViewCell: UICollectionViewCell {
+    
+}
