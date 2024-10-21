af2cb2d4cad12b81469978f7eb993c8169e446b4
sugiyama1692 <kyoei.engine.mba2408.starlight@gmail.com>
2024-10-21 10:29:19 +0900

    遷移前に再生を停止

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
     var nowIndex:Int = 0
     
     var selectedIndex:Int = 0
+    
+    var isPlaying = false
 
     // スライドショーに使用するタイマーを宣言
     var timer: Timer!
@@ -65,10 +67,11 @@ class ViewController: UIViewController {
 
     // 再生ボタンを押した時の処理
     @IBAction func slideShowButton(_ sender: Any) {
+        print("test")
         // 再生中か停止しているかを判定
         if (timer == nil) {
             // 再生時の処理を実装
-
+            isPlaying = true
             // タイマーをセットする
             timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
 
@@ -80,6 +83,7 @@ class ViewController: UIViewController {
 
         } else {
             // 停止時の処理を実装
+            isPlaying = false
             // タイマーを停止する
             timer.invalidate()
 
@@ -106,24 +110,30 @@ class ViewController: UIViewController {
            // indexの画像をstoryboardの画像にセットする
            imageView.image = imageArray[nowIndex]
     }
+    
     @IBAction func onTapImg(_ sender: Any) {
         performSegue(withIdentifier: "extededImage", sender: nil)
     }
     
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if isPlaying {
+            // @IBActionメソッドを呼び出し
+            self.slideShowButton(startButton!)
+        }
+    
         // segueから遷移先のResultViewControllerを取得する
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
         // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
-        selectedIndex = nowIndex
-        var selectedImage = imageArray[nowIndex]
+//        selectedIndex = nowIndex
+        let selectedImage = imageArray[nowIndex]
         resultViewController.image = selectedImage
-        
     }
     
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
-        self.slideShowButton(startButton!)
-        nowIndex = selectedIndex
-        imageView.image = imageArray[nowIndex]
+//        self.slideShowButton(startButton!)
+//        nowIndex = selectedIndex
+//        imageView.image = imageArray[nowIndex]
     }
 
 }
